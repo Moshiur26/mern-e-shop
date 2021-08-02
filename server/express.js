886e4b013375575454803cbe920b300957cc1154
moshiur26 <moshiurrahmanb11@gmail.com>
2021-08-03 04:26:37 +0600

    add required order api

--- server/express.js
+++ server/express.js
@@ -9,6 +9,7 @@ import Template from './../template'
 import devBundle from './devBundle'
 import userRoutes from './routes/user.routes'
 import productRoutes from './routes/product.routes'
+import orderRoutes from './routes/order.routes'
 import authRoutes from './routes/auth.routes'
 import path from 'path'
 
@@ -41,6 +42,7 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use('/', userRoutes)
 app.use('/', authRoutes)
 app.use('/', productRoutes)
+app.use('/', orderRoutes)
 
 // app.get('/', (req, res) => {
 //     res.status(200).send(Template())
---
+++ server/routes/order.routes.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import orderCtrl from '../controllers/order.controller'
+import productCtrl from '../controllers/product.controller'
+import authCtrl from '../controllers/auth.controller'
+import userCtrl from '../controllers/user.controller'
+
+const router = express.Router()
+
+router.route('/api/orders/:userId')
+  .post(authCtrl.requireSignin, productCtrl.decreaseQuantity, orderCtrl.create)
+
+router.route('/api/orders/user/:userId')
+  .get(authCtrl.requireSignin, orderCtrl.listByUser)
+
+router.route('/api/order/status_values')
+  .get(orderCtrl.getStatusValues)
+
+router.route('/api/order/cancel/:productId')
+  .put(authCtrl.requireSignin, productCtrl.increaseQuantity, orderCtrl.update)
+
+// router.route('/api/order/:orderId/charge/:userId/:shopId')
+//   .put(authCtrl.requireSignin, shopCtrl.isOwner, userCtrl.createCharge, orderCtrl.update)
+
+router.route('/api/order/status')
+  .put(authCtrl.requireSignin, orderCtrl.update)
+
+router.route('/api/order/:orderId')
+  .get(orderCtrl.read)
+
+router.route('/api/orders/')
+.get(authCtrl.requireSignin, userCtrl.isAdmin, orderCtrl.list)
+  
+router.param('userId', userCtrl.userById)
+router.param('productId', productCtrl.productByID)
+router.param('orderId', orderCtrl.orderByID)
+
+export default router
